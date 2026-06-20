fbce5c7cbd5cbab87a6abde23b15d91d89efd515
est356 <estherly356@gmail.com>
2026-06-20 09:29:51 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[(int) s.charAt(i) - 97]++;
+            count[(int) t.charAt(i) - 97]--;
+        }
+        for (int i = 0; i < count.length; i++ ){
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
